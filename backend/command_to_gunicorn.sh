1384add7b5d2f2b6527fa6800db648f22f0651f4
Кирилл Антонов <antonov.kirill1991@yandex.ru>
2023-09-15 16:46:48 +0300

    fix Dockerfile

--- backend/command_to_gunicorn.sh
+++ backend/command_to_gunicorn.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 python manage.py migrate
 python manage.py collectstatic
 cp -r /app/collected_static/. /static/static/
-gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
+exec "$@"
